a335e0b0a2782767d06638d3aed03ca2ba28887b
Ro YongHun <mogi3206@naver.com>
2022-08-22 22:31:41 +0900

    Do it Swift Light Button Study

--- ImageView/ViewController.swift
+++ ImageView/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var isZoom = false
+    var imgOn = UIImage?
+    var imgOff = UIImage?
 
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imgOn = UIImage(named: "lamp_on.png")
+        imgOff = UIImage(named: "lamp_off.png")
+    
+        imgView.image = imgOn
+        
     }
 
-
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale:CGFloat = 2.0
+        var newWidth:CGFloat, newHeight:CGFloat
+        
+        if (isZoom) {   //true
+            
+            
+        }
+        else { //false
+            
+        }
+        isZoom = !isZoom?
+        //isZoom뒤에 옵셔널은 다시 삭제해야함
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+    }
 }
 
